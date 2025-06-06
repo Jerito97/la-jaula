fba1d25f9223961beb52c4445cd35e5676f3cf3d
Jerito97 <46766840+Jerito97@users.noreply.github.com>
2025-06-06 14:33:57 -0300

    Update eliminacion.sql

--- database/eliminacion.sql
+++ database/eliminacion.sql
@@ -1,2 +1,16 @@
+USE lajaula;
+
 -- Eliminar una reserva específica
-DELETE FROM reserva WHERE idReserva = 3;
+SET FOREIGN_KEY_CHECKS = 0; 
+-- 1. Eliminar los pagos asociados a la reserva (si existen)
+DELETE FROM
+    pago
+WHERE
+    idReserva = 3;
+-- 2. Eliminar la reserva específica
+DELETE FROM
+    reserva
+WHERE
+    idReserva = 3;
+
+SET FOREIGN_KEY_CHECKS = 1;
